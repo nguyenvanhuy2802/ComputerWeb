aeea9a3849908809e9eb8c01af58373e1d0a12a2
kobitcode77 <21130382@st.hcmuaf.edu.vn>
2025-05-24 16:37:12 +0700

    update api CartItemController

--- src/main/java/com/example/demo/dtos/AuthResponse.java
+++ src/main/java/com/example/demo/dtos/AuthResponse.java
@@ -7,8 +7,8 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthResponse {
+public class AuthResponse<T> {
     private boolean success;
     private String message;
-    private String token;
+    private T data;
 }
